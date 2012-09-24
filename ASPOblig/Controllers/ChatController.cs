1c2e1d2fdfbccf7d880967d45ce82fd8471ca04e
Ken-HAvard Lieng <kk_998@hotmail.com>
2012-09-24 20:13:21 +0200

    Sending av chatmeldinger via databasen fungerer nå sånn høvelig.

--- ASPOblig/Controllers/ChatController.cs
+++ ASPOblig/Controllers/ChatController.cs
@@ -11,9 +11,48 @@ namespace ASPOblig.Controllers
         //
         // GET: /Chat/
 
+        private static int head = 0;
+        
         public ActionResult Index()
         {
             return View();
+        }
+
+        public void Join()
+        {
+            Session["lastRequest"] = DateTime.Now;
+            Session["currentPos"] = 0;
+        }
+
+        public void SendMessage(string message)
+        {
+            if (message != null)
+            {
+                DataClassesDataContext db = new DataClassesDataContext();
+                db.Messages.InsertOnSubmit(new Message { sender = "Herp", destination = "Lobby", message = message, datetime = DateTime.Now });                
+                db.SubmitChanges();
+
+                head++;
+            }
+        }
+
+        public ActionResult GetMessages()
+        {
+            DataClassesDataContext db = new DataClassesDataContext();
+            int currentPos = (int)Session["currentPos"];
+            
+            if (head > currentPos)
+            {
+                var messages = db.Messages.ToList();
+                messages.Reverse();
+                var messages2 = messages.Take(head - currentPos);
+                currentPos += head - currentPos;
+                Session["currentPos"] = currentPos;
+                return Json(messages2, JsonRequestBehavior.AllowGet);
+            }
+
+            //Session["lastRequest"] = DateTime.Now;
+            return Json(null, JsonRequestBehavior.AllowGet);
         }
     }
 }
