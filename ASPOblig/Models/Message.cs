047b02b21f54d727bbd21231170426bd04645085
martinbang <Elexer@gmail.com>
2012-12-12 03:39:44 +0100

    models pølsegreier

--- ASPOblig/Models/Message.cs
+++ ASPOblig/Models/Message.cs
@@ -2,11 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 
 namespace ASPOblig.Models
 {
-    public partial class Message
+    public partial class Message : IMessageService
     {
-       
+      /*  private DataClassesDataContext db;
+
+
+        public Message()
+        {
+            db = new DataClassesDataContext();
+        }
+
+        public List<Message> getAll() 
+        {
+            return db.Messages.ToList();
+        }*/
+
     }
+
+
 }
